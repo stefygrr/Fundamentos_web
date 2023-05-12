d744e8dec546f0e120aee4f0ffaefa93ed7fab18
Stephany camily <stefycamly@gmail.com>
2023-05-11 22:17:22 -0300

    Atualização e js

---
+++ assets/js/script.js
@@ -0,0 +1,85 @@
+//Variaveis de escopo global
+const nome = document.querySelector("#nome");
+const email = document.querySelector("#email");
+
+const cep = document.querySelector("#cep");
+
+let nomeOk = false;
+let emailOk = false;
+
+function validarNome(){
+
+//variavel de escopo local(dentro de um unico local)
+    let txtNome = document.querySelector("#txtNome");
+
+    if(nome.value.length < 3){
+        txtNome.innerHTML = "Nome muito curto!";
+        txtNome.style.color = "red";
+        nomeOk= false
+
+    }else{
+        txtNome.innerHTML = "✔"
+        txtNome.style.color = "green"
+        nomeOk = true
+    }
+}
+
+function validarEmail(){
+
+    let txtEmail = document.querySelector("#txtEmail")
+
+    if(email.value.indexOf("@") === -1 || email.value.indexOf(".") === -1 ){
+        txtEmail.innerHTML = "E-mail inválido"
+        txtEmail.style.color = "red"
+        emailOk = false
+
+    }else{
+        txtEmail.innerHTML = "✔"
+        txtEmail.style.color = "green"
+        emailOk = true
+    }
+}
+
+function validarEmailRegEx(){
+    let regex = /^\w+([.-]?\w+)@\w+([.-]?\w+)(.\w{2,3})+$/
+    let txtEmail = document.querySelector("#txtEmail")
+
+    if(email.value.match(regex)){
+        txtEmail.innerHTML = "✔"
+        txtEmail.style.color = "green"
+        emailOk = true
+    }else{
+        txtEmail.innerHTML = "E-mail inválido"
+        txtEmail.style.color = "red"
+        emailOk = false
+    }
+}
+
+function enviarFormulario(){
+    if(nomeOk === true && emailOk === true){
+        alert(nome.value + ", Obrigada pela sua mensagem!")
+    }else{
+        alert("Por favor, preencha o formulário corretamente!")
+    }
+}
+
+function consultarCep(){
+    console.log(cep)
+    const url = `https://viacep.com.br/ws/${cep.value}/json/`;
+
+    fetch(url)
+    .then((Response) => Response.json())
+    .then((jsonBody) => {
+        document.getElementById("dados").innerHTML = 
+            jsonBody.logradouro + 
+            "\n" +
+            jsonBody.bairro +
+            "\n" +
+            jsonBody.localidade +
+            "\n" +
+            jsonBody.uf
+    })
+    .catch((error)=> {
+        alert("CEP não encontrado!")
+    })
+}
---
+++ teste.js
@@ -0,0 +1,30 @@
+console.log("Hello World!")
+
+let nome= "Stephany"
+
+const sobrenome= "Camily"
+
+console.log(nome + " " + sobrenome)
+
+nome = "Lais"
+
+console.log(nome)
+
+let numero1 = 30
+let numero2 = 10
+
+console.log(numero1 + numero2)
+
+function dividir(num1, num2){
+    return num1/ num2
+}
+
+let resultado = dividir(10, 5)
+console.log(resultado)
+//console.log(dividir(10,5))
+
+function tabuada(num1){
+    for(contador = 1; contador <= 10; contador ++){
+        console.log
+    }
+}
